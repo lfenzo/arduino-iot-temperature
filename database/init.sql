1c956365e772337f896962080b9a5e6b54472a8f
lfenzo <lfenzo@protonmail.com>
2023-02-18 20:09:04 -0300

    feat: funcional from the web-app to the database, from POST request to insertion

--- database/init.sql
+++ database/init.sql
@@ -5,7 +5,8 @@ CREATE TABLE sensor_observations(
     datetime TIMESTAMP NOT NULL,
     device VARCHAR(255) NOT NULL,
     temp_dht11 float NOT NULL,
-    temp_other float NOT NULL,
+    temp_ds18b20 float NOT NULL,
+    humidity_dht11 float NOT NULL,
 
     PRIMARY KEY (datetime, device)
 );
--- server/app.py
+++ server/app.py
@@ -1,9 +1,40 @@
+import toml
+import pg8000.native as pg
+
 from flask import Flask, request
 
+
 app = Flask(__name__)
+database_settings = toml.load("../settings.toml")['database']
+
+
+def __insert_sensor_observations(data: dict):
+    """
+    Insert information contained in 'data' using the 'database_settings'.
+
+    data : dict
+        Python dictionary with the information to be inserted in the
+        'sensor_observations' table.
+    """
+    sql = f"""
+        INSERT INTO public.sensor_observations
+        VALUES (
+            '{data["datetime"]}',
+            '{data["device"]}', 
+            {data["temp_dht11"]}, 
+            {data["temp_ds18b20"]},
+            {data["humidity_dht11"]}
+        );
+        """
+    with pg.Connection(**database_settings) as conn:
+        conn.run(sql)
+
 
 @app.route("/", methods = ["GET", "POST"])
 def generic_handler():
     if request.method == "POST":
-        print(request.form["temp"])
+        print("recebendo requisicao, mandando inserir no banco")
+        __insert_sensor_observations(request.form)
     return {"value": 10}
+
+
