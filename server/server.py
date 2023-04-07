12d0412813c3b843c1ea2143279f4fd64dc0465e
lfenzo <lfenzo@protonmail.com>
2023-04-07 19:37:40 -0300

    introduced fast-api implementation

---
+++ server/server.py
@@ -0,0 +1,47 @@
+import toml
+import pg8000.native as pg
+
+from fastapi import FastAPI, Request
+from pydantic import BaseModel
+
+
+app = FastAPI()
+settings = toml.load("./settings.toml")
+
+
+class TempObs(BaseModel):
+    datetime: str
+    device: str
+    temp: float
+
+
+def __submit_sql_transaction(sql: str):
+    with pg.Connection(**settings['database']) as conn:
+        return conn.run(sql)
+
+
+@app.get("/sensor/", summary="Return all observations between the specified period.")
+async def get_observations_in_period(request: Request):
+    """
+    Parameters
+    ----------
+    `request : Request`
+    """
+    start = request.query_params.get("start")
+    end = request.query_params.get("end")
+    sql = f"""
+        SELECT *
+        FROM public.sensor_observations
+        WHERE datetime BETWEEN '{start}' AND '{end}'
+        """
+    return __submit_sql_transaction(sql)
+
+
+@app.post("/sensor/", summary="Insert a temperature observation.")
+async def insert_observation(obs: TempObs):
+    sql = f"""
+        INSERT INTO public.sensor_observations
+        VALUES ('{obs.datetime}', '{obs.device}', {obs.temp});
+        """
+    __submit_sql_transaction(sql)
+    return obs
